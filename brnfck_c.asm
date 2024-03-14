f2e4d0b9e180f87a7e759ac6d6e26fa59761638b
OSSIRISSA <pipcovich003@gmail.com>
2024-03-14 18:03:03 +0200

    V.4
    - Fully working program, all tests passed.
    - Huge size of .COM, will fix it later

--- brnfck_c.asm
+++ brnfck_c.asm
@@ -1,99 +1,133 @@
 .model tiny
 .data
-    inputFileName db 'TEST4.B', 0  ; Input Brainfuck ode file name
-    readBuffer db 10000 dup(?)  ; Buffer for reading BF code
-    tape db 10000 dup(?)  ; Tape of chars
-
+    fileName DB 20 dup(?)
+    readBuffer DB 10000 dup(?)          ; Buffer for reading BF code
+    tape DW 10000 dup(0)                ; Tape of chars
 .code
 ORG 0100h
 start:
+    lea di, fileName                    ; Load effective address of fileName buffer into DI
+    mov si, 82h                         ; Start after the command length byte
+
+parse_filename:
+    lodsb                               ; Load byte at DS:SI into AL, increment SI
+    cmp al, 0Dh
+    je short finished_parsing
+    stosb                               ; Store AL into ES:DI, increment DI
+    jmp short parse_filename
+
+finished_parsing:
+    xor al, al                          ; Null terminator
+    stosb
+
+    ; Now, attempt to open the file
+    mov ah, 3Dh
+    lea dx, fileName                    ; DX points to the file name
+    int 21h
+    mov bx, ax
+
     mov ax, cs
     mov ds, ax
     
-    ; Open input file for reading
-    mov ah, 3Dh
-    mov al, 0 ; Read mode  
-    lea dx, inputFileName
-    int 21h
-
     ; Read BF code into buffer
-    mov bx, ax
     mov ah, 3Fh
     lea dx, readBuffer
     mov cx, 10000
     int 21h
-    
+
     ;link code to si
-    lea si, readBuffer
-    
-    ; Close file function
-    mov ah, 3Eh
+    lea si, readBuffer  
+
+    ; Close file
+    mov ah, 3Eh         
     int 21h
-    
-    ;link chars to di
-    lea di, tape        
-    
+
+    lea bx, tape
+    xor ax, ax
+clearTapeLoop:
+    mov [bx], ax
+    inc bx
+    inc bx
+    loop clearTapeLoop
+
+    lea di, tape
+
 read_code:
-    mov al, [si]  ; Load current Command into AL
-    cmp al, 0  ; Check end of the code
-    je done
+    mov al, [si]                        ; Load current Command into AL
+    cmp al, 0                           ; Check end of the code
+    je short done
 
     ; dp++
     cmp al, '>'
-    jne check_decrement_pointer
+    jne short check_decrement_pointer
+    inc di
     inc di
     jmp short next_command
 
 check_decrement_pointer:
     ; dp--
     cmp al, '<'
-    jne check_increment_data
+    jne short check_increment_data
+    dec di
     dec di
     jmp short next_command
 
 check_increment_data:
     ; data[dp]++
     cmp al, '+'
-    jne check_decrement_data
-    inc byte ptr [di]
+    jne short check_decrement_data
+    inc word ptr [di]
     jmp short next_command
 
 check_decrement_data:
     ; data[dp]--
     cmp al, '-'
-    jne check_output
-    dec byte ptr [di]
+    jne short check_output
+    dec word ptr [di]
     jmp short next_command
 
 check_output:
-    ; print char
+    ;print char
     cmp al, '.'
-    jne check_input
-    mov dl, [di]
+    jne short check_input
+    mov dx, [di]
+    cmp dx, 0Ah
+    jne short print
+    push dx
+    mov dx, 0Dh
+    mov ah, 02h 
+    int 21h
+    pop dx
+print:
+    cmp dx, 0Dh
+    je next_command
     mov ah, 02h         
     int 21h          
     jmp short next_command
-    
+
 check_input:
     ; read char
     cmp al, ','
-    jne check_loop_begin
-    mov ah, 01h  ; Read input
-    int 21h
-    mov [di], al
+    jne short check_loop_begin
+    mov ah, 3Fh
+    mov bx, 0                           ; stdin handle
+    mov cx, 1                           ; 1 byte to read
+    lea dx, [di]                        ; buffer to read into
+    int 21h                             ; read into buffer
+    or ax, 0                            ; Check if the number of bytes read is 0 (EOF)
+    jnz short next_command              ; If EOF, handle it specifically
+    mov word ptr [di], 0FFFFh           ; EOF
     jmp short next_command
 
 check_loop_begin:
-    cmp al, '['
-    je start_loop
-    jne check_loop_end
     ; loop begin
+    cmp al, '['
+    je short start_loop
 
 check_loop_end:
-    cmp al, ']'
-    je end_loop
-    jne next_command
     ; loop end
+    cmp al, ']'
+    je short end_loop
 
 next_command:
     inc si
@@ -104,52 +138,55 @@ done:
     int 21h
     
 start_loop:
-    cmp byte ptr [di], 0
-    jne loop_continue
-    mov cx, 1
-    
-find_matching_bracket:
-    inc si
-    mov al, [si]
+    cmp word ptr [di], 0
+    jz  find_matching_bracket_forward   ; If zero, we need to find the matching closing bracket
+    jmp next_command                    ; Otherwise, just proceed
+
+find_matching_bracket_forward:
+    mov cx, 1                           ; Level of nesting, starting with 1 for the current loop
+    jmp forward_search_loop
+
+forward_search_loop:
+    inc si                              ; Move to the next character
+    mov al, [si]                               ; Load it into AL
     cmp al, '['
-    je inc_cx
+    je  increase_nesting                ; If we find another '[', increase nesting level
     cmp al, ']'
-    je dec_cx
-    jnz find_matching_bracket
-    jmp read_code
+    je  decrease_nesting                ; If we find a ']', decrease nesting level and check if it's the matching one
+    jmp forward_search_loop             ; Continue searching forward
 
-inc_cx:
-    inc cx
-    jmp find_matching_bracket
+increase_nesting:
+    inc cx                              ; Increase nesting level
+    jmp forward_search_loop
 
-dec_cx:
-    dec cx
-    jnz find_matching_bracket
-    jmp read_code
-
-loop_continue:
-    jmp next_command
-
+decrease_nesting:
+    dec cx                              ; Decrease nesting level
+    jnz forward_search_loop             ; If CX != 0, we're still inside nested loops
+    jmp next_command                    ; Found the matching ']', continue execution
 end_loop:
-    cmp byte ptr [di], 0
-    je next_command
-    mov cx, 1
-find_matching_open_bracket:
-    dec si
-    mov al, [si]
+    cmp word ptr [di], 0
+    jne find_matching_bracket_backward  ; If nonzero, we need to find the matching opening bracket
+    jmp next_command                    ; Otherwise, just proceed
+
+find_matching_bracket_backward:
+    mov cx, 1                           ; Level of nesting, starting with 1 for the current loop
+    jmp backward_search_loop
+
+backward_search_loop:
+    dec si                              ; Move to the previous character
+    mov al, [si]                              ; Load it into AL
     cmp al, ']'
-    je inc_cx_end
+    je  increase_nesting_backward       ; If we find another ']', increase nesting level
     cmp al, '['
-    je dec_cx_end
-    jnz find_matching_open_bracket
-    jmp start_loop
+    je  decrease_nesting_backward       ; If we find a '[', decrease nesting level and check if it's the matching one
+    jmp backward_search_loop            ; Continue searching backward
 
-inc_cx_end:
-    inc cx
-    jmp find_matching_open_bracket
+increase_nesting_backward:
+    inc cx                              ; Increase nesting level
+    jmp backward_search_loop
 
-dec_cx_end:
-    dec cx
-    jnz find_matching_open_bracket
-    jmp start_loop
+decrease_nesting_backward:
+    dec cx                              ; Decrease nesting level
+    jnz backward_search_loop            ; If CX != 0, we're still inside nested loops
+    jmp next_command                    ; Found the matching '[', continue execution
 end start
